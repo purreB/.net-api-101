60c1a84930ffc43567eebf9d3b8221a43e98f38a
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2022-11-01 15:53:20 +0100

    wip champion info model

--- Api/Models/ChampionInfoModel.cs
+++ Api/Models/ChampionInfoModel.cs
@@ -1,15 +1,23 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace Api.Models
 {
-  public class ChampionInfoModel
-  {
-    public int Attack { get; }
-    public int Magic { get; }
-    public int Defense { get; }
-    public int Difficulty { get; }
-  }
+	public class ChampionInfoModel
+	{
+		private int _attack;
+		private int _magic;
+		private int _defense;
+		private int _difficulty;
+
+		public ChampionInfoModel(int attack, int magic, int defense, int difficulty)
+		{
+			_attack = attack;
+			_magic = magic;
+			_defense = defense;
+			_difficulty = difficulty;
+		}
+
+		public int Attack { get => _attack; set => _attack = value; }
+		public int Magic { get => _magic; set => _magic = value; }
+		public int Defense { get => _defense; set => _defense = value; }
+		public int Difficulty { get => _difficulty; set => _difficulty = value; }
+	}
 }
